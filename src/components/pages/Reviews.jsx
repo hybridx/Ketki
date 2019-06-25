89cd71a44764f0727d342ad53f0cf183bf2cc060
magixster <magixster.live@gmail.com>
2019-06-25 19:58:04 +0530

    some changes

--- src/components/pages/Reviews.jsx
+++ src/components/pages/Reviews.jsx
@@ -1,12 +1,82 @@
 import React, { Component } from 'react';
 import Header from '../Header';
+import Footer from '../Footer';
+
 const Reviews = ({}) => (
     <div>
         <Header />
-        <div class="rc-container">
-            <div id="google-reviews"></div>
-            Reviews
-        </div>
+            <div class="rc-container">
+            <div class="grid-1 reviewBox">
+                <div>
+                    <p> Review 1</p>
+                </div>
+            </div>
+
+            <div class="grid-2 reviewBox">
+            <div>
+                    <p> Review 2</p>
+                </div>
+            </div>
+
+            <div class="grid-3 reviewBox">
+                <div>
+                    <p> Review 3</p>
+                </div>
+            </div>
+
+            <div class="grid-4 reviewBox">
+                <div>
+                    <p> Review 4</p>
+                </div>
+            </div>
+            <div class="grid-5 reviewBox">
+                <div>
+                    <p> Review 5</p>
+                </div>
+            </div>
+
+            <div class="grid-6 reviewBox">
+                <div>
+                    <p> Review 6</p>
+                </div>
+            </div>
+
+            <div class="grid-7 reviewBox">
+                <div>
+                    <p> Review 7</p>
+                </div>
+            </div>
+
+            <div class="grid-8 reviewBox">
+                <div>
+                    <p> Review 8</p>
+                </div>
+            </div>
+            <div class="grid-9 reviewBox">
+                <div>
+                    <p> Review 9</p>
+                </div>
+            </div>
+
+            <div class="grid-10 reviewBox">
+            <div>
+                    <p> Review 10</p>
+                </div>
+            </div>
+
+            <div class="grid-11 reviewBox">
+            <div>
+                    <p> Review 11</p>
+                </div>
+            </div>
+
+            <div class="grid-12 reviewBox">
+                <div>
+                    <p> Review 12</p>
+                </div>
+            </div>
+            </div>
+        <Footer />
     </div>
 )
 
